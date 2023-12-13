a187ec6e9d4ac921a500bb0a8f77960d976624b1
Ruchir Mehta <ruchirmehta14@gmail.com>
2023-12-13 11:48:39 -0800

    Update quicksort.py

---
+++ Data Structures/Sorting/quicksort.py
@@ -0,0 +1,14 @@
+"""
+Implement quicksort.
+"""
+
+# Quicksort: Divide and Conquer
+# O(n log n)
+# [5, 2, 6, 4, 1, 3]
+def quicksort(arr):
+    pass
+
+
+arr = [5,2,6,4,1,3]
+quicksort(arr)
+print(arr)
